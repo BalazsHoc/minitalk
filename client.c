11da8dd097133c11d1fb913152c75f6cb4bef2cf
Balazs Hocsak <balazshocsak@Balazss-MacBook-Pro.local>
2024-08-25 12:07:08 +0200

    one signal is sent and catched

--- client.c
+++ client.c
@@ -1,94 +1,20 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bhocsak <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/23 18:17:22 by bhocsak           #+#    #+#             */
-/*   Updated: 2024/08/23 18:17:24 by bhocsak          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
 
-#include <stdio.h> // TAKE THAT OUT
-#include <unistd.h> // TAKE THAT OUT
-#include <signal.h> // TAKE THAT OUT
-
-static int pid_problem(char *pid)
+void	*f(int pid)
 {
-	int i;
+	void	*d;
 
-	i = 0;
-	if (pid[0] == '\0')
-		return (1);
-	while (pid[i])
-	{
-		if (pid[i] < '0' || pid[i] > '9')
-			return (1);
-		i++;
-	}
-	return (0);
-}
-static int	mt_atoi(char *str)
-{
-	int	i;
-	int	pid;
-
-	i= 0;
-	pid = 0;
-	while (str[i++])
-		pid = pid * 10 + str[i] + '0';
-	return (pid);
-}
-
-static void	strlen_sending(int pid, char *str)
-{
-	int	len;
-	int	i;
-
-	len = 0;
-	i = 31;
-	while (str[len])
-		len++;
-	while (i--)
-	{
-		if (len >> i & 1)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-	}
-}
-
-static void	char_sending(int pid, unsigned char text)
-{
-	int	i;
-	int	bit;
-
-	i = 8;
-	while (--i)
-	{
-		bit = (text >> i & 1);
-		if (bit == 1)
-			kill(pid , SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-	}
+	d = 0;
+	kill(pid, SIGUSR1);
+	return (void *)(d);
 }
 
 int main(int argc, char **argv)
 {
-	int	i;
-
-	i = 0;
-	if (argc == 3)
-	{
-		if (pid_problem(argv[1]))
-			return(printf("\nInvalid PID\n"), 0);
-		strlen_sending(mt_atoi(argv[1]), argv[2]);
-		while (argv[2][i++])
-			char_sending(mt_atoi(argv[1]), (unsigned char)argv[2][i]);
-	}
-	else
-		printf("\nPlease type a PID and a message\n");
-	return (0);
+	if (argc == 2)
+		signal(SIGUSR1, f(atoi(argv[1])));
+ 	return (0);
 }
--- server.c
+++ server.c
@@ -1,69 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bhocsak <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/23 18:17:35 by bhocsak           #+#    #+#             */
-/*   Updated: 2024/08/23 18:17:36 by bhocsak          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
 
-#include <stdio.h> // TAKE THAT OUT
-#include <unistd.h> // TAKE THAT OUT
-#include <signal.h> // TAKE THAT OUT
-
-// static void	get_str_len(int len)
-// {
-// 	static int	len;
-
-
-
-// 	len = 0;
-// 	if (sig == SIGUSR1)
-// 		len = len * 2 + 1;
-// 	else
-// 		len = len * 2 + 1;
-// }
-
-static void	print_bits(int sig)
+void	f()
 {
-	static int		i;
-	unsigned char	c;
-
-	i = 7;
-	c = '\0';
-	if (i == 0)
-	{
-		i = 7;
-		c = '\0';
-	}
-	if (i--)
-	{
-		if (sig == SIGUSR1)
-			c = c * 2 + 1;
-		else
-			c = c * 2 + 0;
-	}
-	if (i == 0)
-		write(1, &c, 1);
+	printf("Hell yeah\n");
 }
 
-int main()
+int	main()
 {
-	int	i;
-	int	len;
-
-	i = 31;
-	len = 0;
-    printf("\nPID: %d\n", getpid());
-	//get_str_len(len);
+	printf("PID: %d\n", getpid());
+	signal(SIGUSR1, f);
 	while (1)
-	{
-		signal(SIGUSR1, print_bits);
-		signal(SIGUSR2, print_bits);
-		usleep(1);
-	}
-	return (0);
+		pause();
 }
