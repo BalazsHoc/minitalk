fec59e82f264bde7ce3196436261f93572d848f1
Balazs Hocsak <bhocsak@c4r2p10.42vienna.com>
2024-09-04 15:11:17 +0200

    mandatory bonus

---
+++ bonus/client_utils_bonus.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_utils_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhocsak <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 17:47:50 by bhocsak           #+#    #+#             */
+/*   Updated: 2024/09/03 17:48:12 by bhocsak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "client_bonus.h"
+
+int	mt_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+pid_t	mt_atoi(const char *nptr)
+{
+	int		i;
+	int		sign;
+	long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	if (nptr[i] == '\0')
+		return (0);
+	while (nptr[i] == ' ' || nptr[i] == '\f' || nptr[i] == '\n'
+		|| nptr[i] == '\r' || nptr[i] == '\t' || nptr[i] == '\v')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
---
+++ mandatory/server_utils.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhocsak <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 12:46:47 by bhocsak           #+#    #+#             */
+/*   Updated: 2024/09/03 12:46:49 by bhocsak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "server.h"
+
+struct s_data_s	g_server;
+
+void	whole_text_done(void)
+{
+	g_server.message[g_server.n_char] = '\0';
+	ft_printf("%s\n", g_server.message);
+	if (g_server.message)
+	{
+		free(g_server.message);
+		g_server.message = NULL;
+	}
+	g_server.n_char = -1;
+	g_server.buf_rs = 0;
+}
+
+void	buffer_making(void)
+{
+	char	*temp;
+
+	if (!g_server.message
+		|| g_server.n_char == g_server.buf_rs * g_server.buffer_size)
+	{
+		g_server.buf_rs += 1;
+		temp = malloc(sizeof(char) * ((g_server.buf_rs
+						* g_server.buffer_size) + 1));
+		if (!temp)
+		{
+			if (g_server.message)
+				free(g_server.message);
+			write(2, "Memory allocation error\n", 25);
+			exit(1);
+		}
+		if (g_server.message)
+		{
+			mt_strlcpy(temp, g_server.message, g_server.n_char);
+			free(g_server.message);
+		}
+		g_server.message = temp;
+	}
+}
+
+void	get_bit(int sig, siginfo_t	*siginfo, void *context)
+{
+	static int				i;
+	static unsigned char	c;
+
+	(void)context;
+	if (!i)
+		i = 8;
+	if (i)
+	{
+		if (sig == SIGUSR1)
+			c = c * 2 + 1;
+		else if (sig == SIGUSR2)
+			c = c * 2 + 0;
+		i--;
+		if (i == 0)
+		{
+			if (c == '\0')
+				whole_text_done();
+			else
+				g_server.message[g_server.n_char] = c;
+			g_server.n_char += 1;
+			buffer_making();
+			c = 0;
+		}
+		kill(siginfo->si_pid, SIGUSR1);
+	}
+}
+
+void	handler_server(void)
+{
+	struct sigaction	s_usr;
+
+	g_server.n_char = 0;
+	g_server.buffer_size = 256;
+	g_server.buf_rs = 0;
+	g_server.message = NULL;
+	buffer_making();
+	s_usr.sa_sigaction = get_bit;
+	s_usr.sa_flags = SA_SIGINFO;
+	sigemptyset(&s_usr.sa_mask);
+	sigaction(SIGUSR1, &s_usr, NULL);
+	sigaction(SIGUSR2, &s_usr, NULL);
+}
+
+void	got_sigint(int sig)
+{
+	(void)sig;
+	if (g_server.message)
+		free(g_server.message);
+	ft_printf("\n");
+	exit(1);
+}
